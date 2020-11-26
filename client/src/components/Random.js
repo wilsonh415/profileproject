c35978f678db56ed5df7a60169757682b1d30111
wilsonh415 <wilsonh415@gmail.com>
2020-11-25 23:41:48 -0800

    added favorite albums

--- client/src/components/Random.js
+++ client/src/components/Random.js
@@ -1,18 +1,92 @@
 import React from 'react';
+import { Row, Col, Image } from 'react-bootstrap';
+import tameimpala from '../images/theslowrush.jpg';
+import inreturn from '../images/inreturn.jpg';
+import travisscott from '../images/birdsinthetrap.jpg';
+import rhcp from '../images/stadiumarcadium.jpg';
+
  
 class Random extends React.Component {
    componentDidMount() {
-       console.log("Hello");
+       
    }
  
    render() {
-       const styles = {
-           top: "6vh",
-           position: "relative",
+       const favAlbumStyle = {
+            fontSize: "35px", 
+            fontFamily: "Georgia",
+            marginLeft: "7vw",
+            textAlign: "left",
+            color: "white"
+       }
+        const rowStyle = {
+            top: "3vh",
+            position: "relative",
+            marginLeft: "0vw",
+            fontFamily: "Georgia"
+        };
+       const tameStyles = {
+           top: "2vh",
+           position: "relative"
+       }
+       const odeszaStyles = {
+            top: "2vh",
+            position: "relative"
+       }
+       const textStyle = {
+           fontSize: "19px",
+           color: "white",
+           backgroundColor: "grey",
+           margin: "3vh auto",
+           width: "270px"
+       }
+       const odeszaText = {
+            marginTop: "3vh",
+            fontSize: "19px",
+            color: "white",
+            backgroundColor: "grey",
+            width: "250px",
+            margin: "3vh auto"
        }
        return (
            <div className="randomStyle">
-               <h1 style={styles}>Coming soon...</h1>
+               <h3 style={favAlbumStyle}>Favorite Albums</h3>
+               <div>
+                   <Row style={rowStyle}>
+                       <Col md={5} style={odeszaStyles}>
+                        <a href="https://open.spotify.com/album/5SXT6dwhHX56Sos7KMcMF5" 
+                        target="_blank" rel="noopener noreferrer">
+                            <Image src={inreturn} height={220} width={220}/>
+                        </a>
+                       <b><p style={odeszaText}>ODESZA - In Return</p></b>
+                       </Col>
+                       <Col md={4} style={tameStyles}>
+                        <a href="https://open.spotify.com/album/31qVWUdRrlb8thMvts0yYL" 
+                        target="_blank" rel="noopener noreferrer">
+                            <Image src={tameimpala} height={220} width={220}/>
+                        </a>
+                        <b><p style={textStyle}>Tame Impala - The Slow Rush</p></b>
+                       </Col>
+                   </Row>
+                   <br/>
+                   <Row style={rowStyle}>
+                       <Col md={5} style={tameStyles}>
+                        <a href="https://open.spotify.com/album/42WVQWuf1teDysXiOupIZt" 
+                        target="_blank" rel="noopener noreferrer">
+                            <Image src={travisscott} height={220} width={220}/>
+                        </a>
+                        <b><p style={textStyle}>Travis Scott - Birds in the Trap Sing McKnight</p></b>
+                       </Col>
+                       <Col md={4} style={odeszaStyles}>
+                        <a href="https://open.spotify.com/album/7xl50xr9NDkd3i2kBbzsNZ"
+                        target="_blank" rel="noopener noreferrer">
+                            <Image src={rhcp} height={220} width={220}/>
+                        </a>
+                       <b><p style={textStyle}>Red Hot Chili Peppers - Stadium Arcadium</p></b>
+                       </Col>
+                   </Row>
+
+               </div>
            </div>
        );
    }

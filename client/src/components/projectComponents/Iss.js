11ffd64e5f395015760bc93cb4eecb850a8a9e72
wilsonh415 <wilsonh415@gmail.com>
2020-11-28 13:26:25 -0800

    ISS project upgrades

--- client/src/components/projectComponents/Iss.js
+++ client/src/components/projectComponents/Iss.js
@@ -3,13 +3,15 @@ import { MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import Leaflet from 'leaflet';
 import issimg from '../../images/issicon.png';
+import { Card } from 'react-bootstrap';
 
 class Iss extends React.Component {
     constructor() {
         super();
         this.state = {
             issData: null,
-            position: null
+            position: null,
+            velocity: null
         }
     };
 
@@ -17,7 +19,11 @@ class Iss extends React.Component {
         fetch('/api/issdata')
         .then(resp => resp.json())
         .then(data =>  {
+            console.log(data);
             this.setState({issData: data});
+            this.setState({
+                velocity: (parseInt(data.velocity) / 1.609)
+            });
             this.setState({
                 position: [data.latitude, data.longitude]
             });
@@ -37,21 +43,28 @@ class Iss extends React.Component {
         const styles = {
             top: "3vh",
             position: "relative",
-            // marginLeft: "30vw",
             fontFamily: "Georgia",
         };
         const mapStyle = {
-            marginTop: "50px",
+            marginTop: "20px",
             height: "420px",
             width: "420px",
             marginLeft: "auto",
             marginRight: "auto"
         }
+        const infoStyle = {
+            marginTop: "2vh",
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: "21vw",
+            padding: "0px",
+            background: "lightgrey"
+        }
         return (
             <div className="projectStyle">
                 <div style={styles}>
                     <h3>
-                        <b>Where is the International Space Station right now?
+                        <b>Where is the International Space Station (ISS) right now?
                         </b>
                     </h3>
                     {
@@ -59,10 +72,11 @@ class Iss extends React.Component {
                         <div>
                             Waiting for map to load...
                         </div> :
+                        <div>
                         <MapContainer center={this.state.position}
                         zoom={2} scrollWheelZoom={false} style={mapStyle}>
                             <TileLayer
-                                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                attribution='&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             />
                             <Marker position={this.state.position} icon={issicon}>
@@ -72,6 +86,20 @@ class Iss extends React.Component {
                                 </Popup>
                             </Marker>
                         </MapContainer>
+                        <Card style={infoStyle}>
+                            <Card.Body style={{padding: "0px"}}>
+                                <Card.Title><u>ISS Data: </u></Card.Title>
+                                <ul>
+                                    <li><b>Latitude</b>: {(this.state.position === null) ? 
+                                    null : this.state.position[0].toFixed(5)}°</li>
+                                    <li><b>Longitude</b>: {(this.state.position === null) ? 
+                                    null : this.state.position[1].toFixed(5)}°</li>
+                                    <li><b>Velocity</b>: {(this.state.velocity === null) ?
+                                    null : this.state.velocity.toFixed(2)} mph</li>
+                                </ul>
+                            </Card.Body>
+                        </Card>
+                        </div>
                     }
                 </div>
             </div>

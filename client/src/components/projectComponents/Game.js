29bebefa4f94e96be88b21195684b4cb8cfec97e
wilsonh415 <wilsonh415@gmail.com>
2020-12-07 23:06:59 -0800

    minesweeper grid complete

--- client/src/components/projectComponents/Game.js
+++ client/src/components/projectComponents/Game.js
@@ -15,6 +15,8 @@ class Game extends React.Component {
 
     createGrid = () => {
         let grid = [];
+
+        // initialize the grid
         for(let i = 0; i < 10; i++) {
             grid.push([]);
             for(let j = 0; j < 10; j++) {
@@ -23,9 +25,107 @@ class Game extends React.Component {
                     yPos: j,
                     isMine: false,
                     isClicked: false,
+                    neighbors: null, // aka mines
                     key: i.toString() + j.toString()
                 }
             }
+        }
+        this.initializeMines(grid);
+        this.calculateNeighbors(grid);
+        return grid;
+    }
+
+    calculateNeighbors = (grid) => {
+        for(let i = 0; i < 10; i++) {
+            for(let j = 0; j < 10; j++) {
+                // not a mine
+                let current = grid[i][j];
+                if(!grid[i][j].isMine) {
+                    current.neighbors = 0;
+                    // top left
+                    if(i === 0 && j === 0) {
+                        current.neighbors += (grid[i][j+1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j+1].isMine) ? 1 : 0;
+                    }
+                    // top right
+                    else if(i === 0 && j === 9) {
+                        current.neighbors += (grid[i][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j-1].isMine) ? 1 : 0;
+                    }
+                    // bottom left
+                    else if(i === 9 && j === 0) {
+                        current.neighbors += (grid[i-1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j + 1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i][j+1].isMine) ? 1 : 0;
+                    }
+                    // bottom right
+                    else if(i === 9 && j === 9) {
+                        current.neighbors += (grid[i][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j-1].isMine) ? 1 : 0;
+                    }
+                    // first row, cant access mines above
+                    else if(i === 0) {
+                        current.neighbors += (grid[i][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i][j+1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j+1].isMine) ? 1 : 0;
+                    }
+                    // last row, cant access mines below
+                    else if(i === 9){
+                        current.neighbors += (grid[i][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i][j+1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j+1].isMine) ? 1 : 0;
+                    }
+                    // first column, cant access mines to the left
+                    else if(j === 0) {
+                        current.neighbors += (grid[i-1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j+1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i][j+1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j+1].isMine) ? 1 : 0;
+                    }
+                    // last column, cant access mines to the right
+                    else if(j === 9) {
+                        current.neighbors += (grid[i-1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j-1].isMine) ? 1 : 0;
+                    }
+                    // calculate everything around
+                    else {
+                        current.neighbors += (grid[i-1][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i-1][j+1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i][j+1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j-1].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j].isMine) ? 1 : 0;
+                        current.neighbors += (grid[i+1][j+1].isMine) ? 1 : 0;
+                    }
+                }
+            }
+        }
+        return grid;
+    }
+
+    initializeMines = (grid) => {
+        let seenMines = [];
+        for(let k = 0; k < 14; k++) {
+            let randomX = Math.floor(Math.random() * 10);
+            let randomY = Math.floor(Math.random() * 10);
+            while(seenMines.indexOf({randomX, randomY}) != -1) {
+                randomX = Math.floor(Math.random() * 10);
+                randomY = Math.floor(Math.random() * 10);
+            }
+            seenMines.push({randomX, randomY});
+            grid[randomX][randomY].isMine = true;
         }
         return grid;
     }
@@ -41,11 +141,13 @@ class Game extends React.Component {
                     {
                         row.map(col => {
                             const cellStyle = {
-                                border: "1px solid black"
+                                border: "1px solid black",
+                                width: "35px",
+                                height: "35px"
                             }
                             return (
                                 <td key={col.key} style={cellStyle} onClick={() => this.mineClicked(col.key)}>
-                                    {col.key}
+                                    {(col.isMine) ? "💣" : col.neighbors}
                                 </td>
                             )
                         })
@@ -53,6 +155,10 @@ class Game extends React.Component {
                 </tr>
             )
         })
+    }
+
+    restartGame = () => {
+        window.location.reload();
     }
 
     render() {
@@ -75,7 +181,7 @@ class Game extends React.Component {
                     </tbody>
                 </table>
                 <br/>
-                <Button variant="primary">Restart!</Button>
+                <Button variant="primary" onClick={() => this.restartGame()}>Restart!</Button>
             </div>
         )
     }
